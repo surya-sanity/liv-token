53dd51e878cc693161cd93f915362f148b7238ca
surya-sanity <surya.sanity@gmail.com>
2023-02-04 12:08:19 +0530

    test route & app added

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,14 @@
 const functions = require("firebase-functions");
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+
+app.get("/test", (req, res) => {
+  res.send("Hello from Liv Token 🚀");
+});
+
+exports.app = functions.https.onRequest(app);
